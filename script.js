b77233c52c936fc7c0b6b473c50a9a324af3ae15
Akos Somodi <akossomodi@yahoo.com>
2020-03-18 10:09:08 +0100

    fetch random user from api and take the response

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const sortButton = document.getElementById('sort');
 const calculateWealthButton = document.getElementById('calculate-wealth');
 
 let data = [];
+
+getRandomuser();
+getRandomuser();
+getRandomuser();
+
+// fetch random user and add money
+async function getRandomuser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
+
+function addData(obj) {
+  data.push(obj);
+}
