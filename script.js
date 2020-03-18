9b8ee5b63d118ac4fa9105b5e0fc78c6bf678097
Akos Somodi <akossomodi@yahoo.com>
2020-03-18 10:27:59 +0100

    update the DOM, format the money numbers, clear the main div (js file)

--- script.js
+++ script.js
@@ -28,4 +28,26 @@ async function getRandomuser() {
 
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // clear the main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.getElementById('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money  https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney() {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
